81122101e46db366f7fea3082c4060888b0ed287
MasterJD <35749150+MasterJD@users.noreply.github.com>
2024-06-01 21:50:03 -0600

    Vercel backend change

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -5,3 +5,32 @@
     https://jsonplaceholder.typicode.com/albums
     https://jsonplaceholder.typicode.com/photos
 */
+import express from 'express';
+import axios from 'axios';
+
+/* Using express() to return promise when making a request to the given API's by RELISH */
+const externalApiApp = express();
+
+/* Task 01 - External API Endpoint to provide the photo info, ex. /externalapi/photos/1 */
+externalApiApp.get('/externalapi/photos/:id', async (req, res) => {
+    const idValue = req.params.id;
+
+    try{
+        const photoIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/photos/${idValue}`)
+        const photoIdData = photoIdResponse.data;
+
+        const albumIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums/${photoIdData.albumId}`)
+        const albumIdData = albumIdResponse.data;
+
+        const userIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${albumIdData.userId}`)
+        const userIdData = userIdResponse.data;
+
+        const externalAPIData = {...photoIdData, albumIdData: {...albumIdData, userIdData}};
+
+        res.json(externalAPIData);
+    }
+    catch (error){
+        res.status(500).json({ error: 'Error getting the photo information.' });
+    }
+});
+exports.app = externalApiApp;
