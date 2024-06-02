a851026c627f90b3788395b15882b95b11abff0a
MasterJD <35749150+MasterJD@users.noreply.github.com>
2024-06-01 19:12:45 -0600

    Part_1 - Implementation
    API Implementation

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -1,19 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/* Development-Test - Javier Donado - 2024 */
+/* Backend code for "Part 1: The API Endpoint" assignment */
+/*
+    https://jsonplaceholder.typicode.com/users
+    https://jsonplaceholder.typicode.com/albums
+    https://jsonplaceholder.typicode.com/photos
+*/
