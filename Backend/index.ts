36f0ea90f2640cf41db317f713bebb76b1a01341
MasterJD <35749150+MasterJD@users.noreply.github.com>
2024-06-02 20:55:47 -0600

    Update index.ts

--- Backend/index.ts
+++ Backend/index.ts
@@ -11,16 +11,26 @@ import axios from 'axios';
 /* Using express() to return promise when making a request to the given API's by RELISH */
 const externalApiApp = express();
 
-/* Task 01 - External API Endpoint to provide the photo info, ex. /externalapi/photos/1 - Complete*/
-externalApiApp.get('/externalapi/photos/:id', async (req, res) => {
-    const idValue = req.params.id;
+/* Task 1 - External API Endpoint to provide the photo info, ex. /externalapi/photos/1 - Complete*/
+/* Task 1.2 - The Filtering, implement filters for title, album.title, album.user.email */
+externalApiApp.get('/externalapi/photos', async (req, res) => {
+    //const idValue = req.params.id;
+    const {'title' : titleId} = req.query;
+    const {'album.title' : albumTitleId} = req.query;
+    const {'album.user.email' : albumUserId} = req.query;
 
     try{
-        const photoIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/photos/${idValue}`)
-        const photoIdData = photoIdResponse.data;
+        const photoIdResponse = await axios.get('https://jsonplaceholder.typicode.com/photos');
+        let photoIdData = photoIdResponse.data;
 
-        const albumIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums/${photoIdData.albumId}`)
-        const albumIdData = albumIdResponse.data;
+        //const albumIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums/${photoIdData.albumId}`)
+        //const albumIdData = albumIdResponse.data;
+        const albumRequestPromise = photoIdData.map((photo: { albumId: any; }) => axios.get(`https://jsonplaceholder.typicode.com/albums/${photo.albumId}`));
+        const albumData = await Promise.all(albumRequestPromise);
+
+        albumData.forEach((albumResponse, i) => {
+            photoIdData[i].album = albumResponse.data; 
+        });
 
         const userIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${albumIdData.userId}`)
         const userIdData = userIdResponse.data;
