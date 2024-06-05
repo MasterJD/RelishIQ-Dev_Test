4175407f6c88f88f81eb581e3ceb61cd01c09ded
MasterJD <35749150+MasterJD@users.noreply.github.com>
2024-06-04 22:27:14 -0600

    Task 1.3 - Pagination Complete
    Rewrote code due to slow get requests caused by poor code optimization.

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,26 +1,12 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -35,72 +21,108 @@ Object.defineProperty(exports, "__esModule", { value: true });
 */
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
-//import pLimit from 'p-limit';
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 /* Using pLimit() to limit the amount of data promises fetched by the API request, in this case [25] - Task 1.3 */
 /* Using express() to return promise when making a request to the given API's by RELISH */
 const externalApiApp = (0, express_1.default)();
+const API_URL = process.env.API_URL;
+const calculateSizeInMB = (data) => {
+    const jsonData = JSON.stringify(data);
+    const bytes = Buffer.byteLength(jsonData, 'utf8'); // Measure the byte length of the JSON string
+    const mb = bytes / (1024 * 1024); // Convert bytes to MB
+    return mb;
+};
+const enrichPhotoData = (photo, albumsMap, usersMap) => __awaiter(void 0, void 0, void 0, function* () {
+    const album = albumsMap.get(photo.albumId);
+    const user = usersMap.get(album.userId);
+    return Object.assign(Object.assign({}, photo), { album: Object.assign(Object.assign({}, album), { user }) });
+});
 /* Task 1.1 - External API Endpoint to provide the photo info, ex. /externalapi/photos/1 - COMPLETE */
-externalApiApp.get('/externalapi/photos/:id', async (req, res) => {
+externalApiApp.get('/externalapi/photos/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const idValue = req.params.id;
     try {
-        const photoIdResponse = await axios_1.default.get(`https://jsonplaceholder.typicode.com/photos/${idValue}`);
+        const photoIdResponse = yield axios_1.default.get(`${API_URL}/photos/${idValue}`);
         const photoIdData = photoIdResponse.data;
-        const albumIdResponse = await axios_1.default.get(`https://jsonplaceholder.typicode.com/albums/${photoIdData.albumId}`);
+        const albumIdResponse = yield axios_1.default.get(`${API_URL}/albums/${photoIdData.albumId}`);
         const albumIdData = albumIdResponse.data;
-        const userIdResponse = await axios_1.default.get(`https://jsonplaceholder.typicode.com/users/${albumIdData.userId}`);
+        const userIdResponse = yield axios_1.default.get(`${API_URL}/users/${albumIdData.userId}`);
         const userIdData = userIdResponse.data;
-        const externalAPIData = { ...photoIdData, albumIdData: { ...albumIdData, userIdData } };
+        const externalAPIData = Object.assign(Object.assign({}, photoIdData), { albumIdData: Object.assign(Object.assign({}, albumIdData), { userIdData }) });
+        const dataSizeInMB = calculateSizeInMB(externalAPIData);
+        console.log(`Fetched data size: ${dataSizeInMB.toFixed(2)} MB`);
         res.json(externalAPIData);
     }
     catch (error) {
+        console.error('Error fetching photo information:', error);
         res.status(500).json({ error: 'Error getting the photo information.' });
     }
-});
+}));
 /* Task 1.2 - The Filtering, implement filters for title, album.title, album.user.email - COMPLETE */
-/* Task 1.3 - The Pagination, restrict the amount of data being requested to the API */
-externalApiApp.get('/externalapi/photos', async (req, res) => {
-    const pLimit = (await Promise.resolve().then(() => __importStar(require('p-limit')))).default;
-    const requestLimit = pLimit(5);
+/* Task 1.3 - The Pagination, restrict the amount of data being requested to the API - COMPLETE */
+/* TODO: .ENV file with variables - COMPLETE */
+externalApiApp.get('/externalapi/photos', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    //const pLimit = (await import('p-limit')).default;
+    //const requestLimit = pLimit(5);
     const { 'title': titleId } = req.query;
     const { 'album.title': albumTitleId } = req.query;
     const { 'album.user.email': albumUserEmailId } = req.query;
     const { 'limit': limit } = req.query;
     const { 'offset': offset } = req.query;
+    /* Setting the limits for limit and offset as indicated in Task 1.3 */
+    let limitValue = parseInt(String(limit), 10);
+    let offsetValue = parseInt(String(offset), 0);
     try {
-        /* Setting the limits for limit and offset as indicated in Task 1.3 */
-        let limitValue = parseInt(String(limit), 10);
-        let offsetValue = parseInt(String(offset), 0);
         if (!limitValue && limitValue !== 0) {
             limitValue = 25;
         }
         if (!offsetValue && offsetValue !== 0) {
             offsetValue = 0;
         }
-        const photoIdResponse = await axios_1.default.get('https://jsonplaceholder.typicode.com/photos');
-        let photoIdData = photoIdResponse.data;
-        const albumRequestPromise = photoIdData.map((photo) => requestLimit(() => axios_1.default.get(`https://jsonplaceholder.typicode.com/albums/${photo.albumId}`)));
-        const albumData = await Promise.all(albumRequestPromise);
-        albumData.forEach((albumResponse, i) => {
-            photoIdData[i].album = albumResponse.data;
-        });
-        const userRequesPromise = albumData.map(albumResponse => requestLimit(() => axios_1.default.get(`https://jsonplaceholder.typicode.com/users/${albumResponse.data.userId}`)));
-        const userData = await Promise.all(userRequesPromise);
-        userData.forEach((userResponse, i) => {
-            photoIdData[i].album.user = userResponse.data;
-        });
+        const [photos, albums, users] = yield Promise.all([
+            axios_1.default.get(`${API_URL}/photos`).then(res => res.data),
+            axios_1.default.get(`${API_URL}/albums`).then(res => res.data),
+            axios_1.default.get(`${API_URL}/users`).then(res => res.data)
+        ]);
+        /* After investigating slow get request times optep for Map to group requests (Older version was pretty slow) */
+        const albumsMap = new Map(albums.map((album) => [album.id, album]));
+        const usersMap = new Map(users.map((user) => [user.id, user]));
+        let filteredPhotos = photos;
         if (titleId) {
-            photoIdData = photoIdData.filter((photo) => photo.title.includes(titleId));
+            filteredPhotos = filteredPhotos.filter((photo) => photo.title.includes(titleId));
+            console.log(`Filtered photos by title, remaining count: ${filteredPhotos.length}`);
+            const enrichedDataSizeInMB = calculateSizeInMB(filteredPhotos);
+            console.log(`Enriched data size: ${enrichedDataSizeInMB.toFixed(2)} MB`);
         }
         else if (albumTitleId) {
-            photoIdData = photoIdData.filter((photo) => photo.album.title.includes(albumTitleId));
+            const matchingAlbumIds = albums
+                .filter((album) => album.title.includes(albumTitleId))
+                .map((album) => album.id);
+            filteredPhotos = filteredPhotos.filter((photo) => matchingAlbumIds.includes(photo.albumId));
+            console.log(`Filtered photos by album title, remaining count: ${filteredPhotos.length}`);
+            const enrichedDataSizeInMB = calculateSizeInMB(filteredPhotos);
+            console.log(`Enriched data size: ${enrichedDataSizeInMB.toFixed(2)} MB`);
         }
         else if (albumUserEmailId) {
-            photoIdData = photoIdData.filter((photo) => photo.album.user.email === albumUserEmailId);
+            const matchingUserIds = users
+                .filter((user) => user.email === albumUserEmailId)
+                .map((user) => user.id);
+            const matchingAlbumIds = albums
+                .filter((album) => matchingUserIds.includes(album.userId))
+                .map((album) => album.id);
+            filteredPhotos = filteredPhotos.filter((photo) => matchingAlbumIds.includes(photo.albumId));
+            console.log(`Filtered photos by album user email, remaining count: ${filteredPhotos.length}`);
+            const enrichedDataSizeInMB = calculateSizeInMB(filteredPhotos);
+            console.log(`Enriched data size: ${enrichedDataSizeInMB.toFixed(2)} MB`);
         }
-        res.json(photoIdData.slice(offsetValue, offsetValue + limitValue));
+        const paginatedPhotos = filteredPhotos.slice(offsetValue, offsetValue + limitValue);
+        const enrichedPhotosPromises = paginatedPhotos.map((photo) => enrichPhotoData(photo, albumsMap, usersMap));
+        const enrichedPhotos = yield Promise.all(enrichedPhotosPromises);
+        res.json(enrichedPhotos);
     }
     catch (error) {
+        console.error('Error fetching or processing photos:', error);
         res.status(500).json({ error: 'Error getting the photo information.' });
     }
-});
+}));
 exports.default = externalApiApp;
--- Backend/index.ts
+++ Backend/index.ts
@@ -7,91 +7,127 @@
 */
 import express from 'express';
 import axios from 'axios';
-//import pLimit from 'p-limit';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 /* Using pLimit() to limit the amount of data promises fetched by the API request, in this case [25] - Task 1.3 */
 /* Using express() to return promise when making a request to the given API's by RELISH */
 const externalApiApp = express();
+const API_URL = process.env.API_URL;
 
+const calculateSizeInMB = (data: any): number => {
+    const jsonData = JSON.stringify(data);
+    const bytes = Buffer.byteLength(jsonData, 'utf8'); // Measure the byte length of the JSON string
+    const mb = bytes / (1024 * 1024); // Convert bytes to MB
+    return mb;
+};
+const enrichPhotoData = async (photo: any, albumsMap: Map<number, any>, usersMap: Map<number, any>) => {
+    const album = albumsMap.get(photo.albumId);
+    const user = usersMap.get(album.userId);
+
+    return { ...photo, album: { ...album, user }};
+};
 /* Task 1.1 - External API Endpoint to provide the photo info, ex. /externalapi/photos/1 - COMPLETE */
 externalApiApp.get('/externalapi/photos/:id', async (req, res) => {
     const idValue = req.params.id;
 
     try {
-        const photoIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/photos/${idValue}`);
+        const photoIdResponse = await axios.get(`${API_URL}/photos/${idValue}`);
         const photoIdData = photoIdResponse.data;
 
-        const albumIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums/${photoIdData.albumId}`);
+        const albumIdResponse = await axios.get(`${API_URL}/albums/${photoIdData.albumId}`);
         const albumIdData = albumIdResponse.data;
 
-        const userIdResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${albumIdData.userId}`);
+        const userIdResponse = await axios.get(`${API_URL}/users/${albumIdData.userId}`);
         const userIdData = userIdResponse.data;
 
         const externalAPIData = { ...photoIdData, albumIdData: { ...albumIdData, userIdData } };
 
+        const dataSizeInMB = calculateSizeInMB(externalAPIData);
+        console.log(`Fetched data size: ${dataSizeInMB.toFixed(2)} MB`);
+
         res.json(externalAPIData);
     } catch (error) {
+        console.error('Error fetching photo information:', error);
         res.status(500).json({ error: 'Error getting the photo information.' });
     }
 });
 /* Task 1.2 - The Filtering, implement filters for title, album.title, album.user.email - COMPLETE */
-/* Task 1.3 - The Pagination, restrict the amount of data being requested to the API */
+/* Task 1.3 - The Pagination, restrict the amount of data being requested to the API - COMPLETE */
 
-/* TODO: .ENV file with variables */
+/* TODO: .ENV file with variables - COMPLETE */
 
 externalApiApp.get('/externalapi/photos', async (req, res) => {
-    const pLimit = (await import('p-limit')).default;
-    const requestLimit = pLimit(5);
+    //const pLimit = (await import('p-limit')).default;
+    //const requestLimit = pLimit(5);
 
     const {'title' : titleId} = req.query;
     const {'album.title' : albumTitleId} = req.query;
     const {'album.user.email' : albumUserEmailId} = req.query;
     const {'limit' :  limit} = req.query;
     const {'offset' : offset} = req.query;
+    /* Setting the limits for limit and offset as indicated in Task 1.3 */
+    let limitValue = parseInt(String(limit), 10);
+    let offsetValue = parseInt(String(offset), 0);
 
     try{
-        /* Setting the limits for limit and offset as indicated in Task 1.3 */
-        let limitValue = parseInt(String(limit), 10);
-        let offsetValue = parseInt(String(offset), 0);
-
         if (!limitValue && limitValue !== 0) {
             limitValue = 25;
         }
         if (!offsetValue && offsetValue !== 0) {
             offsetValue = 0;
         }
-
-        const photoIdResponse = await axios.get('https://jsonplaceholder.typicode.com/photos');
-        let photoIdData = photoIdResponse.data;
-
-        const albumRequestPromise = photoIdData.map((photo: { albumId: any; }) => requestLimit(() =>  axios.get(`https://jsonplaceholder.typicode.com/albums/${photo.albumId}`)));
-        const albumData = await Promise.all(albumRequestPromise);
-
-        albumData.forEach((albumResponse, i) => {
-            photoIdData[i].album = albumResponse.data; 
-        });
-
-        const userRequesPromise = albumData.map(albumResponse => requestLimit(() => axios.get(`https://jsonplaceholder.typicode.com/users/${albumResponse.data.userId}`)));
-        const userData = await Promise.all(userRequesPromise);
-
-        userData.forEach((userResponse, i) => {
-            photoIdData[i].album.user = userResponse.data; 
-        });
+        const [photos, albums, users] = await Promise.all([
+            axios.get(`${API_URL}/photos`).then(res => res.data),
+            axios.get(`${API_URL}/albums`).then(res => res.data),
+            axios.get(`${API_URL}/users`).then(res => res.data)
+        ]);
+        /* After investigating slow get request times optep for Map to group requests (Older version was pretty slow) */
+        const albumsMap = new Map<number, any>(albums.map((album: any) => [album.id, album]));
+        const usersMap = new Map<number, any>(users.map((user: any) => [user.id, user]));
+        let filteredPhotos = photos;
 
         if(titleId){
-            photoIdData = photoIdData.filter((photo: { title: any }) => photo.title.includes(titleId));
+            filteredPhotos = filteredPhotos.filter((photo: { title: string }) => photo.title.includes(titleId as string));
+            
+            console.log(`Filtered photos by title, remaining count: ${filteredPhotos.length}`);
+            const enrichedDataSizeInMB = calculateSizeInMB(filteredPhotos);
+            console.log(`Enriched data size: ${enrichedDataSizeInMB.toFixed(2)} MB`);
         }
         else if(albumTitleId){
-            photoIdData = photoIdData.filter((photo: { album: any, title: any }) => photo.album.title.includes(albumTitleId));
+            const matchingAlbumIds = albums
+                .filter((album: { title: string }) => album.title.includes(albumTitleId as string))
+                .map((album: { id: number }) => album.id);
+            filteredPhotos = filteredPhotos.filter((photo: { albumId: number }) => matchingAlbumIds.includes(photo.albumId));
+            
+            console.log(`Filtered photos by album title, remaining count: ${filteredPhotos.length}`);
+            const enrichedDataSizeInMB = calculateSizeInMB(filteredPhotos);
+            console.log(`Enriched data size: ${enrichedDataSizeInMB.toFixed(2)} MB`);
         }
         else if(albumUserEmailId){
-            photoIdData = photoIdData.filter((photo: { album: any, user: any }) => photo.album.user.email === albumUserEmailId);   
+            const matchingUserIds = users
+                .filter((user: { email: string }) => user.email === albumUserEmailId)
+                .map((user: { id: number }) => user.id);
+            const matchingAlbumIds = albums
+                .filter((album: { userId: number }) => matchingUserIds.includes(album.userId))
+                .map((album: { id: number }) => album.id);
+
+            filteredPhotos = filteredPhotos.filter((photo: { albumId: number }) => matchingAlbumIds.includes(photo.albumId)); 
+            console.log(`Filtered photos by album user email, remaining count: ${filteredPhotos.length}`);
+            const enrichedDataSizeInMB = calculateSizeInMB(filteredPhotos);
+            console.log(`Enriched data size: ${enrichedDataSizeInMB.toFixed(2)} MB`);
         }
 
+        const paginatedPhotos = filteredPhotos.slice(offsetValue, offsetValue + limitValue);
+        const enrichedPhotosPromises = paginatedPhotos.map((photo: any) => enrichPhotoData(photo, albumsMap, usersMap));
+        const enrichedPhotos = await Promise.all(enrichedPhotosPromises);
 
-        res.json(photoIdData.slice(offsetValue, offsetValue + limitValue));
+        res.json(enrichedPhotos);
     }
     catch (error){
+        console.error('Error fetching or processing photos:', error);
         res.status(500).json({ error: 'Error getting the photo information.' });
     }
 });
